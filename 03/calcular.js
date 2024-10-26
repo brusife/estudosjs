76a2766abe36081c223d817a841b18822190df6b
Bruno Simon <brusife19@gmail.com>
2024-10-25 22:00:59 -0300

    ex 02 criação

---
+++ 03/calcular.js
@@ -0,0 +1,7 @@
+const n1 = document.getElementById('n1')
+const n2 = document.getElementById('n2')
+const sinal = document.getElementById("sinal")
+
+function Calcular() {
+        alert('OK')
+}
---
+++ 03/mostraoperador.js
@@ -0,0 +1,22 @@
+const op = document.getElementById('res')
+const sinal = document.getElementById('sinal')
+
+function MostraSom() {
+    sinal.innerHTML = "<h2>+</h2>"
+    op.id="resa"
+}
+
+function MostraSub() {
+    sinal.innerHTML = "<h2>-</h2>"
+    op.id="resa"
+}
+
+function MostraMult() {
+    sinal.innerHTML = "<h2>X</h2>"
+    op.id="resa"
+}
+
+function MostraDiv() {
+    sinal.innerHTML = "<h2>/</h2>"
+    op.id="resa"
+}
