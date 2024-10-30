b06736bda8687ef2a5ac4bf5155ae7113f7c04c3
Bruno Simon <brusife19@gmail.com>
2024-10-29 21:34:15 -0300

    çorroco

--- 04/tarefas.js
+++ 04/tarefas.js
@@ -1,20 +1,27 @@
 const lista = []
 const listaf = document.getElementById('lista')
+const check = document.getElementById('conc')
 
 function AdcTarefa() {
     if (tarefa.value !== ""){
         var item = document.createElement('div')
         let tarefa = document.getElementById('tarefa')
-        lista.push(tarefa)
+        lista.push(tarefa.value)
         item.classList.add('item')
-        item.innerHTML = `<div id="conc"></div><div> ${tarefa.value} </div><div><img src="../imgs/remover.png" alt="" onclick="Remover()"></div>`
+        item.innerHTML = `<div id="conc" onclick="Check(this)"></div>
+        <div> ${tarefa.value} </div>
+        <div class="excluir" onclick="Remover()"><img src="../imgs/remover.png" alt=""></div>`
         listaf.appendChild(item)
     }else {
         alert("Digite uma tarefa")
     }
-};
+}
 
 function Remover() {
-    lista.pop()
-    
-}
+    listaf.lastChild.remove(); // Remove elemento HTML
+  }
+
+function Check() {
+    check.innerHTML = `
+        <img src="../imgs/check.png">`
+  }
