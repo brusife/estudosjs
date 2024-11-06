037ab46bce5e6c578159a1aa20a09785ca6d3683
Bruno Simon <brusife19@gmail.com>
2024-11-06 16:56:49 -0300

    tarefas
    quase acabando

--- 04/tarefas.js
+++ 04/tarefas.js
@@ -1,27 +1,50 @@
-const lista = []
-const listaf = document.getElementById('lista')
-const check = document.getElementById('conc')
+let input = document.getElementById('tarefa')
+let btnAdd = document.getElementById('adc')
+let lista = document.getElementById('lista')
+input.focus()
+let cont= 0
 
-function AdcTarefa() {
-    if (tarefa.value !== ""){
-        var item = document.createElement('div')
-        let tarefa = document.getElementById('tarefa')
-        lista.push(tarefa.value)
-        item.classList.add('item')
-        item.innerHTML = `<div class="conc" onclick="Check(this)"></div>
-        <div> ${tarefa.value} </div>`
-        listaf.appendChild(item)
-        tarefa.value = ""
+function Adc(){
+    let inputcont = input.value
+    if (inputcont !=="" && inputcont!==null && inputcont!==undefined) {
+        cont ++
+        let novoitem = `<div id="${cont}" class="item">
+                    <div onclick="Ok(${cont})" class="item-icone">
+                        <span class="mdi mdi-checkbox-blank-outline"></span>
+                    </div>
+                    <div class="item-texto">${inputcont}</div>
+                    <div onclick="Del(${cont})"  class="item-remover">
+                        <span class="mdi mdi-delete"></span>
+                    </div>
+                </div>`
+
+    lista.innerHTML += novoitem
+    input.value = ""
+    input.focus()
     }else {
-        alert("Digite uma tarefa")
+        alert('Por favor, digite uma tarefa, campo vazio... ')
     }
 }
 
-function Check(elemento) {
-    if (elemento.innerHTML== "") {
-        elemento.innerHTML = `
-        <img src="../imgs/check.png">`
-    } else {
-        elemento.innerHTML = ""
+function Del(id){
+    var tarefa = document.getElementById(id)
+    tarefa.remove()
+}
+
+function Ok(id){
+    var item = document.getElementById(id)
+    var classe = item.getAttribute('class')
+    if (classe == "item"){
+        item.classList.add('feito')
+    }else {
+        item.classList.remove('feito')
     }
-  }
+}
+
+
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode == 13){
+        event.preventDefault()
+        btnAdd.click()
+    }
+})
