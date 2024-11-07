552d57e242320f610457c60297d76dfeffbadb01
Bruno Simon <brusife19@gmail.com>
2024-11-07 16:50:38 -0300

    organizando pastas

--- 02/teste.js
+++
@@ -1,5 +0,0 @@
-function desfocar(){
-    let div = document.getElementById("fundo")
-    let sec = document.querySelector('section')
-    sec.classList.add("desfoque")
-}
--- 05/carrossel.js
+++
@@ -1,4 +0,0 @@
-const imagens = document.querySelectorAll('#slide img');
-const indice = 0
-
-function carrossel() {}
