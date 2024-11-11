8f00e505e56846faba5a1f4ad26af69b0fb793ff
Bruno Simon <brusife19@gmail.com>
2024-11-11 14:43:04 -0300

    alterado perfil e estilo mobile, também cores

--- telalogin/tarefas.js
+++ telalogin/tarefas.js
@@ -2,6 +2,7 @@ let aviso = document.getElementById('aviso')
 let input = document.getElementById('name')
 let cont = document.getElementById('conteudo')
 let idades = document.getElementById('nasc')
+let article = document.getElementById('article')
 var passo
 
 for (passo = 1; passo <= 90; passo++) {
@@ -15,9 +16,11 @@ function Mostrar() {
     let tnome = input.value.trim()
     if (tnome !== "" && tnome !== undefined && tnome !== null) {        
     aviso.style.top="0"
-    if (idades.value <=18 || idades.value == "Sua Idade") {
+    if (idades.value <18 || idades.value == "Sua Idade") {
         aviso.innerHTML = `<p><span class="mdi mdi-cancel"></span> Você não tem idade suficiente para ver esses conteúdos...</p>`
     } else {
+        article.style.background="rgb(29, 22, 29)"
+        article.style.animation="none"
         aviso.innerHTML= `
     <img src="../imgs/perfilterror.jpg">
         <p>Bem vindo(a) <span id="enome">${tnome} </span></p>
@@ -42,7 +45,7 @@ function Exibir() {
                 <p>Aproveite seus pesadelos...</p>
                     <img src="../imgs/filmes.jpg" alt="">
             <p>*Esse site é só um estudo e não tem nenhum tipo de conteúdo de filme de terror, ou trecho deles...</p>
-            <button id="entrar" onclick="Sair()">Sair</button>
+            <p><button id="entrar" onclick="Sair()">Sair</button></p>
             </div>`
     }
     
